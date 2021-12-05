ef9db9e0d892b873a86b6ab7c788375df4598c31
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-12-05 12:18:28 +0200

    .

--- web-analysis.js
+++ web-analysis.js
@@ -1,6 +1,5 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-//const url = 'https://www.npmtrends.com';
 const url = 'https://mochajs.org/';
 
 const getUniqueTagsAndTheirAmount = async (urlAddress) => {
@@ -52,71 +51,96 @@ const getUniqueTagsAndTheirAmount = async (urlAddress) => {
 // 				 }).tag
 // 	));
 
+
+
+
 const getLongestPath = async (urlAddress) => {
 	try {
 		const { data } = await axios.get(urlAddress);
 		const $ = cheerio.load(data);
 		var childrenAmount = 0;
-		const longestPath = [];
+		var longestPath = [];
+		var paths = [];
 
-		$('*').each((index, node) => {
 
-			if (node.type === 'tag' && node.children.length !== 0) {
+		console.log($('body').children)
 
-				console.log("TAG WITH CHILDREN: ", node);
-				childrenAmount++;
-				console.log("CHILDREN AMOUNT: ", childrenAmount);
-				longestPath.push(node.name);
-				console.log("FULL PATH: ", longestPath);
-			}
+		// function findAllPaths(startNode, currentCost) {
+	 //        for (var i = 0; i < startNode.children.length; i++) {
+	 //            var child = startNode.children[i];
+	 //            if ( child.next == null ) {
+	 //                paths.push(currentCost);
+	 //            } else {
+	 //                findAllPaths(QATree[child.next], currentCost+1);
+	 //            }
+	 //        }
+	 //    }
+	 //    findAllPaths($('body'), 1);
 
-			return index < 8;
-		});
-
-		return longestPath;
+		return paths;
 	} catch (error) {
 		throw error;
 	}
 };
 
-
 getLongestPath(url);
 
 
 
-	// const buildPath = (node) => {
-	// 	console.log(node);
-	// 	if(node.tagName !== "HTML") {
-	// 	  path.unshift(node.tagName.toLowerCase())
-	// 	  buildPath(node.parentNode)
-	// 	}
-	//   };
-	//   const path = [];
-	//   builtPath(<start node>);
-	//   document.querySelector(path.join(" "))
+
+// DON'T REMOVE
+
+// const getLongestPath = async (urlAddress) => {
+// 	try {
+// 		const { data } = await axios.get(urlAddress);
+// 		const $ = cheerio.load(data);
+// 		var childrenAmount = 0;
+// 		var longestPath = [];
+// 		var paths = [];
+
+// 		$('*').each((index, node) => {
+
+// 			if (node.type === 'tag' && node.children.length !== 0) {
+
+// 				console.log("TAG WITH CHILDREN: ", node);
+// 				childrenAmount++;
+// 				console.log("CHILDREN AMOUNT: ", childrenAmount);
+// 				longestPath.push(node.name);
+// 				console.log("FULL PATH: ", longestPath);
+// 			}
+
+// 			return index < 8;
+// 		});
 
 
-				//const buildPath = (node) => {
-	// 			//console.log(node);
-	// 			if (node.name !== 'html') {
-	// 				console.log(node)
-	// 				// if(node.children !== null) {
-	// 				// 	console.log("CHILD TAG " + children)
-	// 				// 	buildPath(node.parentNode)
-	// 				// }
-	// 			} else {
-	// 				console.log(" HTML")
-	// 				// if(node.children !== null) {
-	// 				// 	console.log("CHILD TAG " + children)
-	// 				// 	//path.unshift(node.tagName.toLowerCase())
-	// 				// 	buildPath(node.parentNode)
-	// 				// }
-	// 			}
+// 		return longestPath;
+// 		return paths;
+// 	} catch (error) {
+// 		throw error;
+// 	}
+// };
 
-	// 		  //};
-	// 	});
+// getLongestPath(url);
 
-	// 	return longestPath;
-	// } catch (error) {
-	// 	throw error;
-	// }
+////
+
+// function shortAndLong(QATree, startNode) {
+//     var paths = [];
+//     function findAllPaths(startNode, currentCost) {
+//         for (var i = 0; i < startNode.answers.length; i++) {
+//             var child = startNode.answers[i];
+//             if (child.nextQuestion == null) {
+//                 paths.push(currentCost);
+//             }else {
+//                 findAllPaths(QATree[child.nextQuestion], currentCost+1);
+//             }
+//         }
+//     }
+//     findAllPaths(startNode, 1);
+//     return [Math.min.apply(Math, paths), Math.max.apply(Math, paths)]
+// }
+// console.debug('ans',shortAndLong(QAndAObj, QAndAObj[1]));//returns [2, 4]
+// console.debug('ans',shortAndLong(QAndAObj, QAndAObj[2]));//returns [1, 3]
+// console.debug('ans',shortAndLong(QAndAObj, QAndAObj[3]));//returns [1, 2]
+// console.debug('ans',shortAndLong(QAndAObj, QAndAObj[4]));//returns [1, 1]
+
